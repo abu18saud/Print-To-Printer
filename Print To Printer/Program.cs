652050b9789b16b46ec2f0f9d3c10876152e8eb4
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-05 07:56:58 +0300

    Done

---
+++ Print To Printer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Print_To_Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            double doubNum = Convert.ToDouble(Console.ReadLine());
+
+
+            Printer.Print(text);
+            Printer.Print(intNum);
+            Printer.Print(doubNum);
+        }
+
+        class Printer
+        {
+            //your code goes here
+            static public void Print<T>(T text)
+            {
+                Console.WriteLine("Showing " + text);
+            }
+        }
+    }
+}
